711d133559ff19caa5ca74017e9cdc26aa0b88fa
leopengilley <you@example.comlionel.pengilley@hotmail.com>
2021-02-13 12:48:31 +1100

    multiple buttons hardcode1

--- src/components/Drumpad.js
+++ src/components/Drumpad.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import UIfx from 'uifx';
-import tickAudio from './my-sounds/kick.mp3';
+import kickAudio from './my-sounds/kick.mp3';
+import hhclosedAudio from './my-sounds/HHclosed.mp3';
+import hhopenAudio from './my-sounds/HHopen.mp3';
+import snareAudio from './my-sounds/snare.mp3';
+
 
 const kick = new UIfx(
-  tickAudio,
+  kickAudio,
   {
     volume: 0.4, // number between 0.0 ~ 1.0
     throttleMs: 100
