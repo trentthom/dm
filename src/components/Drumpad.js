9be5c376891194264385a9125d4742ec3dba7751
trentthom <trent.tt@gmail.com>
2021-02-18 17:17:53 +1100

    get bank name working

--- src/components/Drumpad.js
+++ src/components/Drumpad.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom'
 import Loop from './Loop.js'
 
 const SERVER_URL = 'https://drum-machine-server.herokuapp.com/sounds.json'
+const SERVER_DP = 'https://drum-machine-server.herokuapp.com/drumpads.json'
 
 class Drumpad extends React.Component {
   constructor(){
@@ -12,7 +13,10 @@ class Drumpad extends React.Component {
     this.state = {
       displayText: '', // For the drum machine screen
       padState: 3, // Selecting drumpad groups from the backend
-      sounds: [] // The drumpad group selected
+      sounds: [], // The drumpad group selected
+      firstPadBank: 3,
+      lastPadBank: 5,
+      padBankName: ''
     }
 
     this.playAudio = this.playAudio.bind(this); // Playing sounds on click
@@ -20,6 +24,7 @@ class Drumpad extends React.Component {
     this.onKeyUp = this.onKeyUp.bind(this); // Stopping sounds on key release
     this.changePad = this.changePad.bind(this); // TODO: Changing the grou of selected drumpads
     this.soundsData = this.soundsData.bind(this); // TODO: Changing the grou of selected drumpads
+    this.drumPadData = this.drumPadData.bind(this)
 
   }
 
@@ -27,6 +32,20 @@ class Drumpad extends React.Component {
     axios.get(SERVER_URL).then((response) => {
       const padData = _.where(response.data,{ drumpad_id: padState })
       this.setState({sounds: padData})
+    })
+  }
+
+  drumPadData(drumpadIndex = 0) { // Gets data from backend and puts it into state
+    axios.get(SERVER_DP).then((response) => {
+    const drumPadId = response.data[0].id
+    const drumBankLast = response.data[response.data.length - 1].id
+    const getBankName = response.data[drumpadIndex].name
+
+    console.log(response.data[drumpadIndex].name)
+    this.setState({padState: drumPadId})
+    this.setState({firstPadBank: drumPadId})
+    this.setState({lastPadBank: drumBankLast})
+    this.setState({padBankName: getBankName})
     })
   }
 
@@ -61,7 +80,7 @@ class Drumpad extends React.Component {
     document.addEventListener('keydown', this.onKeyDown);
     document.addEventListener('keyup', this.onKeyUp);
     this.soundsData(this.state.padState); // Runs the get request
-    // console.log(this.state.sounds[1]);
+    this.drumPadData()
   }
 
   componentWillUnmount(){ // Not sure. Fill me in.
@@ -81,29 +100,42 @@ class Drumpad extends React.Component {
   }
 
   changePad(event) {
-    this.setState({padState: 4}); // hardcoded '4'
+    if(this.state.padState < this.state.lastPadBank) {
+      this.setState({padState: this.state.padState + 1});
+
+    } else {
+      this.setState({padState: this.state.firstPadBank})
+
+    }
+    this.drumPadData(this.state.padState)
     this.soundsData(this.state.padState); // Runs the get request
   }
 
   render() {
     return (
-      <div className="outterdiv">
-        <div className="soundname">{ this.state.displayText }</div>
-        <div className="row">
-          {this.state.sounds.map((sound) => // Mapping the drum pads on the screen.
-            <button onClick={this.playAudio} key={sound.id} className="pinkButton" id={sound.name}>
-              {sound.key_trigger}
-              <audio className={sound.id}>
-                <source src={sound.url}></source>
-              </audio>
-            </button>
-          )}
+      <div>
+        <div className="outterdiv">
+          <div className="soundname">{ this.state.displayText }
+          </div>
+          <div className="row">
+            {this.state.sounds.map((sound) => // Mapping the drum pads on the screen.
+              <button onClick={this.playAudio} key={sound.id} className="pinkButton" id={sound.name}>
+                {sound.key_trigger}
+                <audio className={sound.id}>
+                  <source src={sound.url}></source>
+                </audio>
+              </button>
+            )}
+          </div>
+          <div>
+            <Loop />
+          </div>
+          <div>
+            <button onClick={this.changePad}>Change Banks</button>
+          </div>
         </div>
         <div>
-          <Loop />
-        </div>
-        <div>
-          <button onClick={this.changePad}>Change Pad</button> //TODO
+        {this.state.padBankName}
         </div>
       </div>
     )
